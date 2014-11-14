c44e3f1abc08d92e38a4bfc0b8de406244cb3487
Jean-Baptiste Brasselet <jeanbaptiste.brasselet@planorama.com>
2014-11-14 15:56:57 +0100

    correction

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
-var NOOT = require('noot')('core-object');
+/**
+ * Dependencies
+ */
+var NOOT = require('noot')('object');
 var Through2 = require('through2');
 
-var Stdout = NOOT.CoreObject.extend({
-  writableStream: '',
-  _writableStream:'',
-  stdout: '',
-  duplex: '',
+/***********************************************************************************************************************
+ *  Stdout
+ ***********************************************************************************************************************
+ *
+ * @description Permit to write logs in a stream through console.log
+ *
+ *
+ **********************************************************************************************************************/
+var Stdout = NOOT.Object.extend({
+  writer: null,
+  _writableStream: null,
+  _duplex: null,
 
+
+  /**
+   * Constructor
+   */
   init: function() {
-    this.setWritableStream(this.writableStream);
+    this.setWritableStream(this.writer);
+    this.duplicate();
   },
 
-  setWritableStream: function(writableStream) {
+  /**
+   * Define the writableStream
+   *
+   * @param {function} writer
+   */
+
+  setWritableStream: function(writer) {
 
     this._writableStream = new Through2(function(chunk, enc, cb) {
-      writableStream(chunk.toString());
-      cb();
+      writer(chunk.toString('utf8'));
+      return cb();
     });
-  },
-
-  redirect: function () {
-
-    this.duplex = new Through2();
-
-    console._stdout = this.duplex;
-    this.duplex.pipe(process.stdout);
-    this.duplex.pipe(this._writableStream);
 
   },
 
-  destroy: function () {
-    console._stdout = process.stdout;
-    this._writableStream.end();
-    this.duplex.end();
+  /**
+   * Logic to duplicate the log in another stream
+   */
+  duplicate: function () {
+    this._duplex = new Through2();
+    console._stdout = this._duplex;
+    this._duplex.pipe(process.stdout);
+    this._duplex.pipe(this._writableStream);
+
   }
-
 }, {
   instance: null,
 
+  /**
+   * Override method create to allow Singleton pattern
+   *
+   * @returns {Object}
+   */
   create: function() {
     if (this.instance !== null)
       return this.instance;
@@ -48,4 +69,7 @@ var Stdout = NOOT.CoreObject.extend({
   }
 });
 
+/**
+ * @module
+ */
 module.exports = Stdout;
